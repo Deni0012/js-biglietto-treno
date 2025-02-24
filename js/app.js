d3aa62de1a1ffc038c4745435884ff75c28942a6
Deni0012 <deni.dg00@icloud.com>
2025-02-24 16:17:59 +0100

    feat: aggiunta degli sconti

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ const age = prompt("Inserisci la tua età:");
 // Definiamo il prezzo al km
 const prezzoKm = 0.21;
 let prezzoTotale = km * prezzoKm;
+
+// Applichiamo gli sconti
+if (age < 18) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * 20) / 100; // Sconto del 20%
+} else if (age >= 65) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * 40) / 100; // Sconto del 40%
+}
